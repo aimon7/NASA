31cf151bb10657e7998142dce644d1f19f004c33
aimon7 <air.ioannis@gmail.com>
2019-11-22 14:12:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 	</style>
 </head>
 <body>
-	<h1>NASA Rover Coordinates Input Page</h1>
+	<h1>You can find the GitHub repooritory and all files in <a href = "https://github.com/aimon7/NASA">here</a></h1>
 
 	<form action="rover_guide.php" method="POST">
 		<div>
